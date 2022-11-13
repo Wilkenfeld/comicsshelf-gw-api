c6bf7f91a0f9ee8fb8e7bbac6b116ab0b597ee64
Wilkenfeld <pomiato.roberto@gmail.com>
2022-11-13 18:28:23 +0100

    fix port

--- src/main.go
+++ src/main.go
@@ -1,16 +1,21 @@
-package main;
+package main
 
 import (
-	"github.com/gofiber/fiber/v2"
 	"log"
 	"os"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	log.Print("Starting server...")
 	app := fiber.New()
-	app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	log.Fatal(app.Listen(os.Getenv("PORT")))
 }
