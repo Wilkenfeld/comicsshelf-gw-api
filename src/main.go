453c9df893b8adcea325c1afea570519c99759eb
Wilke <34282087+Wilkenfeld@users.noreply.github.com>
2022-11-13 17:57:38 +0100

    First commit

---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main;
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func main() {
+	log.Print("Starting server...")
+	app := fiber.New()
+	app.Get("/", func (c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+	log.Fatal(app.Listen(":3000"))
+}
